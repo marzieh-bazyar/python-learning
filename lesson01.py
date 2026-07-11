656fc855260c4015f8b4b2302281813235d329c7
Marzieh Bazyar <marziehbazyar@gmail.com>
2026-07-11 21:49:41 +0330

    توضیح تغییرات

--- lesson01.py
+++ lesson01.py
@@ -1,13 +1,13 @@
-��
-py
-cd /d D:\pythonlearning
-copy con lesson01.py
-python
-print("���� ������")
- a= 20
->>> b= 87
->>> a,b= b,a
->>> print(a)
-87
->>> print(b)
-20
+players= ['Neymar', 'Ronaldo', 'Messi', 'Holland', 'Cortoa']
+print(players)
+print(players[:3])
+print(players[2:])
+print(players[-1])
+print(players[-3])
+print(players[0:3])
+print(players[0:5])
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+for player in players[2:5]:
+    print(player.title())
